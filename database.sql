13fafbf08d67b34bcb19686dce67c30b47762539
Jennifer Hightower <jenniferhightower2020@gmail.com>
2022-10-13 08:27:22 -0500

    created ibd_dog database and initial SQL queries for tables

--- database.sql
+++ database.sql
@@ -8,3 +8,34 @@ CREATE TABLE "user" (
     "username" VARCHAR (80) UNIQUE NOT NULL,
     "password" VARCHAR (1000) NOT NULL
 );
+
+CREATE TABLE "dog" (
+  "id" SERIAL PRIMARY KEY,
+  "name" VARCHAR(40) NOT NULL,
+  "birthday" DATE NOT NULL,
+  "image" VARCHAR(300),
+  "user_id" INT REFERENCES "user" NOT NULL
+);
+
+CREATE TABLE "symptom" (
+  "id" SERIAL PRIMARY KEY,
+  "appetite" INT NOT NULL,
+  "energy" INT NOT NULL,
+  "stomach_pain" INT NOT NULL,
+  "vomit" INT NOT NULL,
+  "diarrhea" INT NOT NULL,
+  "med_given" BOOLEAN DEFAULT FALSE,
+  "score" INT,
+  "created_at" TIMESTAMP NOT NULL DEFAULT NOW(),
+  "dog_id" INT REFERENCES "dog" NOT NULL
+);
+
+CREATE TABLE "medication" (
+  "id" SERIAL PRIMARY KEY,
+  "name" VARCHAR(60) NOT NULL,
+  "dosage" VARCHAR(20) NOT NULL,
+  "frequency" VARCHAR(40) NOT NULL,
+  "created_at" TIMESTAMP NOT NULL DEFAULT NOW(),
+  "active" BOOLEAN DEFAULT TRUE,
+  "dog_id" INT REFERENCES "dog" NOT NULL
+);
